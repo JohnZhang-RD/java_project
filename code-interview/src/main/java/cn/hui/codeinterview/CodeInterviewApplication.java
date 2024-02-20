23fa38bcd9a85c7008b52ee286289f7058382c34
junhui <419297954@qq.com>
2024-02-20 18:32:42 +0800

    新建项目

---
+++ code-interview/src/main/java/cn/hui/codeinterview/CodeInterviewApplication.java
@@ -0,0 +1,17 @@
+package cn.hui.codeinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zhang Junhui
+ */
+
+@SpringBootApplication
+public class CodeInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeInterviewApplication.class, args);
+    }
+
+}
---
+++ code-interview/src/test/java/cn/hui/codeinterview/CodeInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hui.codeinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
